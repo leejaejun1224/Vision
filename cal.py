639712819f56ea252a43a620d09ccabf7acb56b9
janggune <poroparo@naver.com>
2022-09-30 16:55:30 +0900

    +&- def통일

--- cal.py
+++ cal.py
@@ -1,6 +1,8 @@
-#첫번쨰 입력값을 x, 두번째 입력값(부호)를 sign
-#세번째 입력값을 y, 연산결과를 result 변수에 저장함.
-if sign == "+":
-    result = x+y
-if sign == "-":
-    result = x-y
+#첫번쨰 입력값을 x,#세번째 입력값을 y,
+# 각 함수는 연산결과를 반환함.
+
+def plus(x,y):
+    return x+y
+
+def minus(x,y):
+    return x-y
